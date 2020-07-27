7e168bb0e25257c6d1cecd9a9c44da81f3c48b08
AbdullahAlHarun-code <aharun46@gmail.com>
2020-07-28 00:36:46 +0100

    initial set up

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import Flask, render_template, redirect, request, url_for, flash, jsonify
 
@@ -6,10 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello mama'
+    return 'hello mama valo aso'
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     	port=int(os.environ.get('PORT')),
     	debug=True)
-
