86f16dfbb7338d8d0ffd4e1911996d88b3ef911d
AbdullahAlHarun-code <aharun46@gmail.com>
2020-07-28 03:45:52 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'heroku working and finally and have fun adn fun'
-
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    	port=int(os.environ.get('PORT')),
-    	debug=True)
+from application import app
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "asdfaqw37894598wergfjkdf"
+
+    MONGODB_SETTINGS = {'db' : 'book_shop', 'host' : 'mongodb+srv://root:Zee9Zee9@firstcluster.qhtcj.mongodb.net/book_shop?retryWrites=true&w=majority'
+
+    }
